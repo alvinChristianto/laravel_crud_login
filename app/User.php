e2ea494086be521618d5d39195cd839ad3676adf
alvin <alvinchristianto93@gmail.com>
2019-01-15 11:00:31 +0700

    edit model User.php for table users

--- app/User.php
+++ app/User.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
+	protected $table = 'users';
     protected $fillable = array (
     	'role_id', 'username','name', 'email', 'password'
     );
